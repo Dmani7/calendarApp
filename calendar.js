7e64e71776d69ee8833a9b9e9b0a4d885321e6ff
Dmani7 <35098025+Dmani7@users.noreply.github.com>
2019-06-03 15:44:46 +0300

    Add files via upload

---
+++ calendar.js
@@ -0,0 +1,28 @@
+
+
+function selected_day() {
+    // var myButtonClasses = document.getElementById("btn1").classList;
+    var my_Selected_day = document.getElementById("btn1").classList;
+    // querySelecterAll
+  
+    if (my_Selected_day.contains("my-day")) {
+
+        my_Selected_day.remove("my-day");
+
+    } else {
+
+        my_Selected_day.add("my-day");
+
+    }
+    if (my_Selected_day.contains("my-day-selected")) {
+
+        my_Selected_day.remove("my-day-selected");
+
+    } else {
+
+        my_Selected_day.add("my-day-selected");
+
+    }
+}
+
+// document.querySelector('.week-day').addEventListener('click', my_Selected_day);
